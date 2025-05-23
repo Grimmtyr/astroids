f747901cf61c25bd1fffb89bdee2b86e33fdf1db
Grimmtyr <axelben@outlook.com>
2025-05-23 16:33:14 +0000

    Added player and functional movement

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    delta_time = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
     
     game_on = True
     while game_on:
@@ -11,7 +16,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.update(delta_time)
+        player.draw(screen)
         pygame.display.flip()
+#        print(delta_time)
+        delta_time = game_clock.tick(60) / 1000
    
 
 if __name__=="__main__":
